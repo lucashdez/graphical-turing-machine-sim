08ec5e1a67e2ad075296d5637c457f5e2bd270a0
lucashdez <lukastpiano@gmail.com>
2024-06-03 16:22:19 +0100

    Added base types

--- src/include/base.h
+++ src/include/base.h
@@ -3,5 +3,17 @@
 #ifndef BASE_H
 #define BASE_H
 
+typedef unsigned char u8;
+typedef unsigned int u16;
+typedef unsigned long int u32;
+typedef unsigned long long u64;
+
+typedef signed char i8;
+typedef int i16;
+typedef long int i32;
+typedef long long i64;
+
+typedef float f32;
+typedef double f64;
 
 #endif //BASE_H
